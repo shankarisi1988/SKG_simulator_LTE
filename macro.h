1a80b77032edb313fec688b76c097ffd75f6af81
SHANKAR KUMAR GHOSH <52414033+shankarisi1988@users.noreply.github.com>
2019-08-16 16:58:24 +0530

    Add files via upload

---
+++ macro.h
@@ -0,0 +1,12 @@
+ #define NO_OF_UE 2000
+ #define TTI  2500
+ #define DATA_RATE 256  // in Mbps 
+ #define hard_handoff true  // Either hard_handoff or enable_DC is true
+ #define enable_DC false 
+ #define CBR 9   // CBR traffic arrival rate
+ #define SUBCARRIER 1000  // No. of sub-carrier per BS 
+ #define RBSIZE 12 // No. of subcarriers per resource block
+ #define SYMBOL 6 // No. of OFDMA symbols per LTE-A frame
+ #define MODULATION 1 // 1 for BPSK, 2 for QPSK, 4 for 16-QAM  and 6 for 64-QAM
+ #define FRAME 1   // Framelength in milli-second
+
